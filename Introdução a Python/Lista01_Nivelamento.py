7b64d0378f91c3dcd038b87ce3c691be39d85592
Joel Fentes <82660102+JoelFentes@users.noreply.github.com>
2024-02-21 18:46:12 -0300

    update

--- Introdução a Python/Lista01_Nivelamento.py
+++ Introdução a Python/Lista01_Nivelamento.py
@@ -176,7 +176,6 @@ if contador == 3 or contador == 4:
 if contador == 5:
     print("Você está classificado como ASSASSINO")
 
-
 """ 16. Leia a idade e o tempo de serviço de um trabalhador e escreva se ele pode ou
 não se aposentar. As condições para aposentadoria são: - Ter pelo menos 65 anos, - Ou ter trabalhado pelo menos 30 anos, - Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos. """
 age = int(input("Digite sua idade: "))
