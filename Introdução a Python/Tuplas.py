02ff0a51e3436f41d13171a71451d43f36ed8f64
Joel Fentes <82660102+JoelFentes@users.noreply.github.com>
2024-02-21 21:01:19 -0300

    Exercícios Tuplas

--- Introdução a Python/Tuplas.py
+++ Introdução a Python/Tuplas.py
@@ -1,3 +1,5 @@
+
+'''
 tuple = ()
 
 tuple = tuple + (1,)
@@ -10,3 +12,38 @@ print(type(tuple))
 tuple[1][2] = 100
 
 print(tuple)
+'''
+
+""" 
+tupla = ()
+
+tupla = tupla + ('nome': "Joel", 'idade': 18,)
+
+print(tupla) 
+"""
+
+#1.
+
+""" tupla = (1, 3, 5, 2, 3, 5, 1, 1, 3,)
+
+new_tupla = ()
+
+
+for i in tupla:
+    if i not in new_tupla:
+        new_tupla = new_tupla + (i,)
+ """
+#2.
+
+"""         
+list = [1, 2, 3]
+new_lista = []
+
+for x in list:
+    new_lista.append((x, x**3))
+print(new_lista)
+
+ """
+
+#3.
+
